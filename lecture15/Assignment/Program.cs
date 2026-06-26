851500817815f0bfb9e7d3d467d004f8654a2c27
abashidzeluka <abashidze2003@gmail.com>
2026-06-26 20:05:15 +0400

    Initial commit

---
+++ lecture15/Assignment/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student students = new Student();
+            string chooser = "1";
+            while (chooser != "5")
+            {
+                Console.WriteLine("\n--- menu ---");
+                Console.WriteLine("1. Print all students");
+                Console.WriteLine("2. Add a student");
+                Console.WriteLine("3. Find a student");
+                Console.WriteLine("4. Update a student's score");
+                Console.WriteLine("5. Exit");
+
+                Console.WriteLine();
+                Console.Write("Choose a number from menu: ");
+                chooser = Console.ReadLine();
+
+                switch (chooser)
+                {
+                    case "1":
+                        students.PrintInfo();
+                        break;
+                    case "2":
+                        students.StudentAdd();
+                        break;
+                    case "3":
+                        students.FindStudent();
+                        break;
+                    case "4":
+                        students.UpdateScore();
+                        break;
+                    case "5":
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default: Console.WriteLine("Invalid choice!"); break;
+                }
+            }
+        }
+    }
+}
---
+++ lecture15/Assignment/Student.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment
+{
+    internal class Student
+    {
+        public Student()
+        {
+            Names();
+            Scores();
+        }
+
+        List<string> studentNames = new List<string>();
+        Dictionary<string, int> studentScores = new Dictionary<string, int>();
+        public void Names()
+        {
+            studentNames.Add("Jack");
+            studentNames.Add("Emma");
+            studentNames.Add("Liam");
+            studentNames.Add("Olivia");
+            studentNames.Add("Noah");
+            studentNames.Add("Ava");
+            studentNames.Add("Ethan");
+            studentNames.Add("Sophia");
+            studentNames.Add("Mason");
+            studentNames.Add("Isabella");
+        }
+
+        public void Scores()
+        {
+            studentScores.Add(studentNames[0], 45);
+            studentScores.Add(studentNames[1], 78);
+            studentScores.Add(studentNames[2], 92);
+            studentScores.Add(studentNames[3], 61);
+            studentScores.Add(studentNames[4], 55);
+            studentScores.Add(studentNames[5], 88);
+            studentScores.Add(studentNames[6], 73);
+            studentScores.Add(studentNames[7], 49);
+            studentScores.Add(studentNames[8], 95);
+            studentScores.Add(studentNames[9], 67);
+        }
+
+        public void StudentAdd()
+        {
+            Console.Write("Please enter student name: ");
+            string name = Console.ReadLine().Trim();
+            name = char.ToUpper(name[0]) + name.Substring(1).ToLower();
+
+            Console.Write("Please enter student score: ");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            studentNames.Add(name);
+            studentScores.Add(name, score);
+        }
+
+        public void FindStudent()
+        {
+            Console.Write("Enter student name to find: ");
+            string name = Console.ReadLine().Trim();
+            for (int i = 0; i < studentNames.Count; i++)
+            {
+                if (studentNames[i].ToLower() == name.ToLower())
+                {
+                    Console.WriteLine($"Name: {studentNames[i]} Score: {studentScores[studentNames[i]]}");
+                    return;
+                }
+            }
+            Console.WriteLine("Student not found.");
+        }
+
+        public void UpdateScore()
+        {
+            Console.Write("Please enter student's name: ");
+            string name = Console.ReadLine().Trim();
+            name = char.ToUpper(name[0]) + name.Substring(1).ToLower();
+
+            Console.Write("Please enter new score: ");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < studentNames.Count; i++)
+            {
+                if (name.ToLower() == studentNames[i].ToLower())
+                {
+                    studentScores[studentNames[i]] = score;
+                    Console.WriteLine("Student's info updated successfully!");
+                    return;
+                }
+            }
+            Console.WriteLine("Student not found!");
+
+        }
+
+        public void PrintInfo()
+        {
+           foreach (var item in studentScores)
+    {
+        Console.WriteLine($"Name: {item.Key} Score: {item.Value}");
+    }
+        }
+
+    }
+}
